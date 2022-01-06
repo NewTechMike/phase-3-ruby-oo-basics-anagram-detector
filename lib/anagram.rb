dbe67de364eb6f797f3177be0ce8b8c9ce22f134
Michael Harris <xero5k@yahoo.com>
2022-01-06 10:50:21 -0600

    commit tree 2f4d2d71981ba8b30c91547e4c94baa42170a3b6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word="listen")
+    @word = word
+  end 
+
+  def match word_array
+    first_word = @word.chars
+    second_array = []
+    word_array.each do |word2|
+      if first_word.sort == word2.chars.sort
+        second_array << word2
+      end
+    #binding.pry
+    end 
+    second_array
+  end 
+end 
